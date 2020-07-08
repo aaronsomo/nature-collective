eee6bea9d02483b4898c6e5cf136ee06d39603ef
Aaron Southammavong <aaroneight07@gmail.com>
2020-07-08 10:33:55 -0500

    refactored codebase to use react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import RenderImageCards from './components/RenderImageCards';
 import Input from './components/Input';
 import queryAPI from './utilities/queryAPI';
@@ -8,88 +8,72 @@ import NoResults from './components/NoResults';
 import SelectedTitles from './components/SelectedTitles';
 import './App.scss';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.starterText = 'Nothing Here Yet - Try Searching For An Image!';
-    this.nothingFoundText = 'Nothing Found! Try Another Query.';
-    this.state = {
-      loading: false,
-      error: false,
-      results: [],
-      noResultsText: this.starterText,
-      selectedTitles: [],
-    };
-  }
+const App = () => {
+  const starterText = 'Nothing Here Yet - Try Searching For An Image!';
+  const nothingFoundText = 'Nothing Here Yet - Try Another Query.';
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [results, setResults] = useState([]);
+  const [noResultsText, setNoResultsText] = useState(starterText);
+  const [selectedTitles, setSelectedTitles] = useState([]);
 
-  handleSubmitQuery = (input) => {
-    this.setState({ results: [] });
+  const handleSubmitQuery = (input) => {
+    setResults([]);
     if (input !== '') {
-      this.setState({ loading: true, error: false });
-      return queryAPI(input, this.handleSuccess, this.handleError);
+      setLoading(true);
+      setError(false);
+      return queryAPI(input, handleSuccess, handleError);
     } else {
-      this.setState({ error: 'Please provide a search query first' });
+      setError('Please provide a search query first');
     }
   };
 
-  handleError = (err) => {
-    this.setState({ error: err, loading: false });
+  const handleError = (err) => {
+    setError(err);
+    setLoading(false);
   };
 
-  handleSuccess = (result) => {
-    this.setState({
-      loading: false,
-      results: result,
-      noResultsText: this.nothingFoundText,
-    });
+  const handleSuccess = (result) => {
+    setLoading(false);
+    setResults(result);
+    setNoResultsText(nothingFoundText);
   };
 
-  handleSelect = (selected) => {
-    if (this.state.selectedTitles.includes(selected) === false) {
-      this.setState({
-        selectedTitles: [...this.state.selectedTitles, selected],
-      });
+  const handleSelect = (selected) => {
+    if (selectedTitles.includes(selected) === false) {
+      setSelectedTitles([...selectedTitles, selected]);
     } else {
-      let tempList = this.state.selectedTitles;
+      let tempList = selectedTitles;
 
       tempList = tempList.filter(function(array) {
         return array !== selected;
       });
-      this.setState({
-        selectedTitles: tempList,
-      });
+      setSelectedTitles(tempList);
     }
   };
 
-  clearApp = () => {
-    this.setState({ error: false, results: [] });
+  const clearApp = () => {
+    setError(false);
+    setResults([]);
   };
 
-  render() {
-    let { results, loading, error, noResultsText, selectedTitles } = this.state;
-    return (
-      <div className="App">
-        <h1 className="app-title">Nature Collective</h1>
-        <Input onSubmit={this.handleSubmitQuery} clearApp={this.clearApp} />
+  return (
+    <div className="App">
+      <h1 className="app-title">Nature Collective</h1>
+      <Input onSubmit={handleSubmitQuery} clearApp={clearApp} />
+      {results.length > 0 && (
+        <SelectedTitles selectedTitles={selectedTitles} results={results} />
+      )}
+      {error && <Error error={error} />}
+      {loading && <Loader />}
+      <section className="image-results">
         {results.length > 0 && (
-          <SelectedTitles selectedTitles={selectedTitles} results={results} />
+          <RenderImageCards array={results} handleSelect={handleSelect} />
         )}
-        {error && <Error error={error} />}
-        {loading && <Loader />}
-        <section className="image-results">
-          {results.length > 0 && (
-            <RenderImageCards
-              array={results}
-              handleSelect={this.handleSelect}
-            />
-          )}
-          {results.length === 0 && !loading && (
-            <NoResults text={noResultsText} />
-          )}
-        </section>
-      </div>
-    );
-  }
-}
+        {results.length === 0 && !loading && <NoResults text={noResultsText} />}
+      </section>
+    </div>
+  );
+};
 
 export default App;
