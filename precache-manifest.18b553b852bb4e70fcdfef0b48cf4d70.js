d8e2f9265c9e41be495b3836c2cede760010730d
Aaron Southammavong <aaroneight07@gmail.com>
2020-07-12 21:16:04 -0500

    Updates

---
+++ precache-manifest.18b553b852bb4e70fcdfef0b48cf4d70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bff5bca38ad1f440935",
+    "url": "/nature-collective/static/js/runtime~main.3bff5bca.js"
+  },
+  {
+    "revision": "805782114d2672500c75",
+    "url": "/nature-collective/static/js/main.80578211.chunk.js"
+  },
+  {
+    "revision": "aeb587517ae4e13280b7",
+    "url": "/nature-collective/static/js/1.aeb58751.chunk.js"
+  },
+  {
+    "revision": "805782114d2672500c75",
+    "url": "/nature-collective/static/css/main.1d97ccd2.chunk.css"
+  },
+  {
+    "revision": "04080a7b15d65461105480683944bbe0",
+    "url": "/nature-collective/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/aaronsomo/nature-collective/precache-manifest.2f0002d5d498930d2e5c2a4b02a76b90.js"
+  "/nature-collective/precache-manifest.18b553b852bb4e70fcdfef0b48cf4d70.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/aaronsomo/nature-collective/index.html", {
+workbox.routing.registerNavigationRoute("/nature-collective/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
